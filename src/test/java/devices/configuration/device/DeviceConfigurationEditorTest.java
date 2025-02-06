4a515e7372cfa0cc8e66a76c0c6a7e8f93f67cb2
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2025-02-06 08:36:53 +0100

    initial test of device configuration

--- src/test/java/devices/configuration/device/DeviceConfigurationEditorTest.java
+++ src/test/java/devices/configuration/device/DeviceConfigurationEditorTest.java
@@ -1,2 +1,101 @@
-package devices.configuration.device;public class DeviceConfigurationEditorTest {
+package devices.configuration.device;
+
+import devices.configuration.JsonAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DeviceConfigurationEditorTest {
+    private final String deviceId = "ALF-98262561";
+
+    @Test
+    void createNewDevice() {
+        // given - prepare state of system before test
+
+        // when - execute some functionality under test
+        DeviceConfigurationEditor editor = DeviceConfigurationEditor.createNewDevice(deviceId);
+
+        // then - verify state of the system after test
+        JsonAssert.assertThat(editor)
+                .isExactlyLike("""
+                        {
+                          "deviceId": "ALF-98262561",
+                          "ownership": {
+                            "operator": null,
+                            "provider": null
+                          },
+                          "location": null,
+                          "openingHours": {
+                            "alwaysOpen": true
+                          },
+                          "settings": {
+                            "autoStart": false,
+                            "remoteControl": false,
+                            "billing": false,
+                            "reimbursement": false,
+                            "showOnMap": false,
+                            "publicAccess": false
+                          }
+                        }
+                        """);
+    }
+
+    @Test
+    void changeOwnership() {
+        Assertions.fail("Not Implemented");
+    }
+
+    @Test
+    void setLocation() {
+        Assertions.fail("Not Implemented");
+    }
+
+    @Test
+    void changeSettings() {
+        Assertions.fail("Not Implemented");
+    }
+
+    @Test
+    void fullyConfiguredDevice() {
+        // given
+        DeviceConfigurationEditor editor = DeviceConfigurationEditor.createNewDevice(deviceId);
+
+        // when
+        // change Ownership
+        // set Location
+        // change some Settings
+
+        // then
+        JsonAssert.assertThat(editor)
+                .isExactlyLike("""
+                        {
+                          "deviceId": "ALF-98262561",
+                          "ownership": {
+                            "operator": "Devicex.nl",
+                            "provider": "public-devices"
+                          },
+                          "location": {
+                            "street": "Rakietowa",
+                            "houseNumber": "1A",
+                            "city": "Wrocław",
+                            "postalCode": "54-621",
+                            "country": "POL",
+                            "coordinates": {
+                              "longitude": 16.931752852309156,
+                              "latitude": 51.09836221719513
+                            }
+                          },
+                          "openingHours": {
+                            "alwaysOpen": true
+                          },
+                          "settings": {
+                            "autoStart": false,
+                            "remoteControl": false,
+                            "billing": false,
+                            "reimbursement": false,
+                            "showOnMap": false,
+                            "publicAccess": false
+                          }
+                        }
+                        """);
+    }
 }
